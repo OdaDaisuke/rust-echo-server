041292b440116d971baba72360b6df5346fa2fb1
oda-daisuke <oda-daisuke@kcgrp.jp>
2021-06-27 13:09:05 +0900

    modularize tcp

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,7 @@
 use std::env;
-use std::net::{TcpListener, TcpStream};
-use std::thread;
-use std::io::Read;
-use std::io::Write;
 
+mod tcp;
 mod udp;
-
-fn handle_client(mut stream: TcpStream) {
-    // read 128 bytes at a time from stream echoing back to stream
-    loop {
-        let mut read_buf = [0; 1024];
-        match stream.read(&mut read_buf) {
-            Ok(n) => {
-                if n == 0 { 
-                    // connection was closed
-                    break;
-                }
-                stream.write(&read_buf[0..n]).unwrap();
-            }
-            Err(err) => {
-                panic!(err);
-            }
-        }
-    }
-}
-
-fn run_tcp() {
-    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
-
-    // 中でacceptとかしてくれてるはず
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                thread::spawn(move || {
-                    handle_client(stream);
-                });
-            }
-            Err(_) => {
-                println!("Error");
-            }
-        }
-    }
-}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -50,6 +10,6 @@ fn main() {
         udp::run();
     } else {
         println!("Run TCP echo server");
-        run_tcp();
+        tcp::run();
     }
 }
---
+++ src/tcp.rs
@@ -0,0 +1,41 @@
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::io::Read;
+use std::io::Write;
+
+fn handle_client(mut stream: TcpStream) {
+  // read 128 bytes at a time from stream echoing back to stream
+  loop {
+      let mut read_buf = [0; 1024];
+      match stream.read(&mut read_buf) {
+          Ok(n) => {
+              if n == 0 { 
+                  // connection was closed
+                  break;
+              }
+              stream.write(&read_buf[0..n]).unwrap();
+          }
+          Err(err) => {
+              panic!(err);
+          }
+      }
+  }
+}
+
+pub fn run() {
+  let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+  // 中でacceptとかしてくれてるはず
+  for stream in listener.incoming() {
+      match stream {
+          Ok(stream) => {
+              thread::spawn(move || {
+                  handle_client(stream);
+              });
+          }
+          Err(_) => {
+              println!("Error");
+          }
+      }
+  }
+}
